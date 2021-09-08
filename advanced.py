d549a05f9ea0b1b5dc92b8379182381f60735a7e
ronanhansel <ronanhansel@gmail.com>
2021-09-08 22:25:38 +0700

    added feature to test all available mirrors

---
+++ advanced.py
@@ -0,0 +1,33 @@
+import os, wget
+from termcolours import bcolors
+
+def pacman_all_mirrors() -> list:
+    u = 'y'
+    if os.path.exists('pacmanmirrorlist'):
+        print('pacmanmirrorlist exists, do you want to redownload? (y/n)')
+        u = input()
+    if u.lower() == "y":
+        try:
+            os.remove('pacmanmirrorlist')
+        except OSError:
+            print('No pacmanmirrorlist detected, retrieving file')
+        print('Connecting to https://archlinux.org')
+        wget.download(
+            'https://archlinux.org/mirrorlist/?country=all&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on')
+        if os.path.exists('download.wget'):
+            os.rename('download.wget', 'pacmanmirrorlist')
+        else:
+            raise ValueError('Cannot find downloaded data')
+        print()
+        print('Downloaded, proccessing data')
+        print('Done, saved in ' + os.getcwd())
+    elif u.lower() == "n":
+        print('Reading locally saved file in ' + bcolors.HEADER + os.getcwd() + bcolors.ENDC)
+    with open('pacmanmirrorlist') as p:
+        mirror = p.read().split('\n')
+        mirrors = list(n.replace("#Server = ", "")[:n.index("/$") - 9] for n in list(
+            m for m in mirror if '##' not in m and '!' not in m) if len(n) >= 3)
+    print(bcolors.HEADER + 'Finished proccessing mirrors, now all of them will be sorted accordingly\nThis should take a while but no longer than ' +
+           f'{len(mirrors)*13} seconds\nYou can also disable unwanted mirrors by adding ' + bcolors.ENDC +
+           bcolors.WARNING+ '!' + bcolors.ENDC + bcolors.HEADER +' at the begining of the line' + bcolors.ENDC)
+    return mirrors
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 
-from ctypes import ArgumentError
-import os
-import sys
+
+import os, sys, requests
+from advanced import pacman_all_mirrors
 from time import time
 from multiprocessing import Process, Value
+from ctypes import ArgumentError
+from termcolours import bcolors
+
+# For better user experience when using the script, when debugging this line should be commented
+# but this must not be commented upon commit
+sys.tracebacklimit = 0
+
+# initialise dicts
 
 results = {}
 sort = {}
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
+prompt = bcolors.HEADER + "⇁ " + bcolors.ENDC
+
+def print_list(arr: list):
+    for i, k in enumerate(arr):
+        print(prompt + f"{i}. {k}")
+        
 
 # This can stay intact, this function is simply for receiving test package ie alacritty
 def get(n, start, lat, d, link):
-    import requests
     file_name = "data.temp"
     print("Testing " + bcolors.OKBLUE +
           bcolors.BOLD + d + bcolors.ENDC)
@@ -44,90 +48,99 @@ def get(n, start, lat, d, link):
                 n.value = dl
                 f.write(data)
                 done = int(50 * dl / total_length)
-                sys.stdout.write("\r[%s%s] %.2f kB/%.2f kB" % ('=' *
+                sys.stdout.write("\r[%s%s] %.2f kB/%.2f kB" % ('█' *
                                  done, ' ' * (50-done), (dl/1024), (total_length/1024)))
                 sys.stdout.flush()
 
 # This is where you can modify to further support more package managers
 def run(arg):
-    # You can add more preset packages here
-    pkgs = ['chaotic-aur/x86_64/alacritty-git-0.9.0.1850.g4a3bcdb4-1-x86_64.pkg.tar.zst',
-            'community/os/x86_64/alacritty-0.9.0-1-x86_64.pkg.tar.zst']
-    dire = '/etc/pacman.d/' # Default mirror directory for pacman, you shouldn't change this
-    quest = input("Your mirrors' directory is: " + bcolors.HEADER + dire + bcolors.ENDC + " Correct? \n(y/n): ")
-    # Selection menus
-    while True:
-        if quest.lower() == "y":
-            break
-        elif quest.lower() == "n":
-            dire = input('input yours instead: ')
-            if dire == '/etc/pacman.d/': print("That's just the same xD")
-            break
-        else:
-            print("Please enter a valid option")
-            quest = input(": ")
-    files = os.listdir(dire)
-    for i, k in enumerate(files):
-        print(f"{i}. {k}")
-    print("Type any letter to exit")
-    opt = input("Select test mirror file: ")
     try:
-        opt = int(opt)
-    except Exception:
-        return
-    files = files[opt]
-    for i, k in enumerate(pkgs):
-        print(f"{i}. {k}")
-    print(bcolors.WARNING + "If there's any error with your test, this might be why. Try changing package to see if it's the problem " +
-          "\nOr you can search manually and paste the url here, " + 
-          "\nplease note the valid url would look something like $repo/os/$package or $repo/$package" +
-           bcolors.BOLD + "\nDO NOT" + bcolors.ENDC + bcolors.WARNING + " include host url" + bcolors.ENDC)
-    try:
-        usr = input("Select test packages url(integer) or enter your own(string): ")
-        target = int(usr)
-        pkg = pkgs[target]
-    except Exception:
-        pkg = usr
-    with open(f'{dire}{files}', 'r') as file:
-        data = file.read().replace('\n', ';').replace(";;", ";")
+        if not 'a' in arg:
+            dire = '/etc/pacman.d/' # Default mirror directory for pacman, you shouldn't change this
+            quest = input("Your mirrors' directory is: " + bcolors.HEADER + dire + bcolors.ENDC + " Correct? \n(y/n): ")
+            # Selection menus
+            while True:
+                if quest.lower() == "y":
+                    break
+                elif quest.lower() == "n":
+                    dire = input('input yours instead: ')
+                    if dire == '/etc/pacman.d/': print("That's just the same xD")
+                    break
+                else:
+                    print("Please enter a valid option")
+                    quest = input(": ")
+            files = os.listdir(dire)
+            print_list(files)
+            print("Type any letter to exit")
+            opt = input("Select test mirror file: ")
+            try:
+                opt = int(opt)
+            except Exception:
+                return
+            files = files[opt]
+            with open(f'{dire}{files}', 'r') as file:
+                data = file.read().replace('\n', ';').replace(";;", ";")
 
-    data = data.split(";")
-    data = list(a.replace("Server = ", "").replace(" ", "")[:a.index('$') - 9]
-                for a in list(i for i in data if i != "") if a[0] != "#")
-
-    for d in data:
-        num = Value('d', 0.0)
-        start = Value('d', 0.0)
-        latency = Value('d', 0.0)
-        p = Process(target=get, args=(num, start, latency, d, d + pkg))
-        p.start()
-        p.join(timeout=13)
-        p.terminate()
-        end = time()
-        l = latency.value
-        delta = end - start.value
-        speed_pure = int(num.value/1024) / delta
-        speed = "%.2f kB/sec" % (
-            speed_pure) if speed_pure < 1024 else "%.2f MB/sec" % (speed_pure / 1024)
-        # The connection is marked as Failed after 13 seconds if there's no response
-        if speed_pure == 0.0:
-            speed = bcolors.FAIL + bcolors.BOLD + "Failed" + bcolors.ENDC
-            l = ""
+            data = data.split(";")
+            data = list(a.replace("Server = ", "").replace(" ", "")[:a.index('$') - 9]
+                        for a in list(i for i in data if i != "") if a[0] != "#")
         else:
-            l = "{:.2f}".format(latency.value)
-        print(f" -- {speed}")
-        # Initialise the dictionary
-        results[d] = {
-            'latency': l,
-            'speed': 0
-        }
-        results[d]['speed'] = speed
-        if arg == '-s':
-            sort[d] = speed_pure
-        elif arg == '-l':
-            sort[d] = latency.value
-        
-    return dict(sorted(sort.items(), key=lambda item: item[1])), results
+            print('Advanced mode enabled, testing all available mirrors in https://archlinux.org')
+            data = pacman_all_mirrors()
+            print('Do you want to continue? (any/n)')
+            ip = input()
+            if ip == "n":
+                return
+        # You can add more preset packages here
+        pkgs = ['chaotic-aur/x86_64/alacritty-git-0.9.0.1850.g4a3bcdb4-1-x86_64.pkg.tar.zst',
+                'community/os/x86_64/alacritty-0.9.0-1-x86_64.pkg.tar.zst']
+        print_list(pkgs)
+        print(bcolors.WARNING + "If there's any error with your test, this might be why. Try changing package to see if it's the problem " +
+            "\nOr you can search manually and paste the url here, " + 
+            "\nplease note the valid url would look something like $repo/os/$package or $repo/$package" +
+            bcolors.BOLD + "\nDO NOT" + bcolors.ENDC + bcolors.WARNING + " include host url" + bcolors.ENDC)
+        try:
+            usr = input("Select test packages url(integer) or enter your own(string): ")
+            target = int(usr)
+            pkg = pkgs[target]
+        except Exception:
+            pkg = usr
+        for d in data:
+            num = Value('d', 0.0)
+            start = Value('d', 0.0)
+            latency = Value('d', 0.0)
+            p = Process(target=get, args=(num, start, latency, d, d + pkg))
+            p.start()
+            p.join(timeout=13)
+            p.terminate()
+            end = time()
+            l = latency.value
+            delta = end - start.value
+            speed_pure = int(num.value/1024) / delta
+            speed = "%.2f kB/sec" % (
+                speed_pure) if speed_pure < 1024 else "%.2f MB/sec" % (speed_pure / 1024)
+            # The connection is marked as Failed after 13 seconds if there's no response
+            if speed_pure == 0.0:
+                speed = bcolors.FAIL + bcolors.BOLD + "Failed" + bcolors.ENDC
+                l = ""
+            else:
+                l = "{:.2f}".format(latency.value)
+            print(f" -- {speed}")
+            # Initialise the dictionary
+            results[d] = {
+                'latency': l,
+                'speed': 0
+            }
+            results[d]['speed'] = speed
+            if 's' in arg:
+                sort[d] = speed_pure
+            elif 'l' in arg:
+                sort[d] = latency.value
+            
+        return dict(sorted(sort.items(), key=lambda item: item[1])), results
+    except KeyboardInterrupt:
+        print('Interrupted')
+        return dict(sorted(sort.items(), key=lambda item: item[1])), results
 
 
 if __name__ == "__main__":
@@ -135,15 +148,15 @@ if __name__ == "__main__":
         raise ArgumentError('Missing argument')
     else:
         arg = sys.argv[1]
-        if not arg in ["-l", "-s"]:
+        if not arg[1] in ["l", "s"]:
             raise ArgumentError("Incorrect argument passed")
     place = run(arg)
     print(bcolors.HEADER + bcolors.BOLD + "{:<3} {:<40} {:<20} {}".format(
         'N', 'Mirrors', 'Avg speed', 'Latency') + bcolors.ENDC)
     try:
-        if arg == "-s":
+        if "s" in arg:
             pl = reversed(place[0])
-        elif arg == "-l":
+        elif "l" in arg:
             pl = place[0]
         for i, k in enumerate(pl):
             print("{:<3} {:<40} {:<20} {}" .format(
